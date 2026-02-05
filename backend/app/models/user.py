cb839c8f8877c9ae2c089eacbf726f18339c030c
Edu <eduladron@gmail.com>
2026-02-05 14:54:21 +0100

    registering now uses UUID 4 on user.id

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,9 +1,10 @@
 from ..extensions import db
+import uuid
 
 class User(db.Model):
     __tablename__ = 'user'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     username = db.Column(db.String, nullable=False, unique=True)
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
