ec01031489e0660e4bce22a50db89f4458e707c4
Edu <eduladron@gmail.com>
2026-05-06 16:53:35 +0200

    clean config.py

--- backend/config.py
+++ backend/config.py
@@ -8,10 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent
 load_dotenv(BASE_DIR / ".env")
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL", "postgresql://eduladron:12341234@localhost:5432/vtt_db")
+    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = os.getenv("FLASK_DEBUG", "True").lower() == "true"
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super_secret_key")
+    DEBUG = os.getenv("FLASK_DEBUG", "False").lower() == "true"
+    JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
